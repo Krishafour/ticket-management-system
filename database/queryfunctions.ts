c38ee5e9264efe56eb514fc27cc69d831bf94e03
Krishna <krishna.poul@afourtech.com>
2022-04-04 13:47:05 +0530

    04-04-2022  commit

--- database/queryfunctions.ts
+++ database/queryfunctions.ts
@@ -1,39 +1,14 @@
 import { QueryResult} from "pg";
 import { pool } from "./db";
 
-export function databaseOperation(Query: string, array?:((string | Date | undefined|boolean) [] )| string ): Promise<QueryResult>{
-    // const ans:QueryResult = await client1.query(Query, array);
-    // return ans;
-    return new Promise(async(resolve,reject)=>{
+export let databaseOperation=async(Query: string, array?:((string | Date | undefined|boolean) [] )| string ): Promise<QueryResult>=>{
         try{
-            
             const ans:QueryResult = await pool.query(Query, array);
-            resolve(ans);
+            return ans;
          }
-        catch(err:any|unknown)
+        catch(err:unknown)
         {
-            return reject(err);
+            throw err;
         }
-    })
+    
 }
-// export function allTicketHistory(token:tokenData):Promise<ticketInfoOutput>{
-//     return new Promise(async(resolve,reject)=>{
-//         try{
-            
-//             if(token.role==ROLE.USER)
-//             {
-//                 const ticketInfo:QueryResult= await databaseOperation(TICKETS_TABLE_QUERIES.TICKET_INFORMATION_HISTORY_OF_USER,[token.user_id,false,TICKET_STATUS.APPROVED,TICKET_STATUS.REJECTED]);
-//                 return resolve({status:RESPONSE_STATUS.SUCCESS,ticket:ticketInfo.rows});
-//             }
-//             else
-//             {
-//                 const ticketInfo:QueryResult=await databaseOperation(TICKETS_TABLE_QUERIES.TICKET_INFORMATION_HISTORY,[TICKET_STATUS.APPROVED,TICKET_STATUS.REJECTED,false]);
-//                 return resolve({status:RESPONSE_STATUS.SUCCESS,ticket:ticketInfo.rows});
-//             }
-//          }
-//         catch(err:any|unknown)
-//         {
-//             return reject(err);
-//         }
-//     })
-// }
